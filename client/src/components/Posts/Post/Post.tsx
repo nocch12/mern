4e24400533617036fbaa4179572112c36db185a6
Kohki Ohno <k.ohno12.gt@gmail.com>
2020-12-26 00:04:19 +0900

    CardMediaエラー解消

--- client/src/components/Posts/Post/Post.tsx
+++ client/src/components/Posts/Post/Post.tsx
@@ -12,7 +12,7 @@ import {
   Delete as DeleteIcon,
   MoreHoriz as MoreHorizIcon,
 } from "@material-ui/icons";
-import moment from 'moment';
+import moment from "moment";
 import useStyles from "./styles";
 
 import { IPost } from "../../../reducers/posts";
@@ -25,25 +25,33 @@ const Post: React.FC<Props> = ({ post }) => {
   const classes = useStyles();
   return (
     <Card className={classes.card}>
-      <CardMedia
-        className={classes.media}
-        image={post.selectedFile}
-        title={post.title}
-      />
+      {post.selectedFile ?? (
+        <CardMedia
+          className={classes.media}
+          image={post.selectedFile}
+          title={post.title}
+        />
+      )}
       <div className={classes.overlay}>
         <Typography variant="h6">{post.creator}</Typography>
-        <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
       </div>
       <div className={classes.overlay2}>
-        <Button style={{color: 'white'}} size="small" onClick={() => {}}>
+        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
           <MoreHorizIcon fontSize="default" />
         </Button>
       </div>
       <div className={classes.details}>
-        <Typography variant="body2" color="textSecondary">{post.tags.map(tag => `#${tag} `)}</Typography>
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
       </div>
       <CardContent>
-        <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+        <Typography className={classes.title} variant="h5" gutterBottom>
+          {post.message}
+        </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
         <Button size="small" color="primary" onClick={() => {}}>
